927b02383954d70357e11b57fff9bf5dd9b79ab6
vrushabh-mowade <vrushabhmowade@gmail.com>
2024-04-04 09:39:56 +0530

    1 commit

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import { Card } from "@repo/ui/card";
+import { Code } from "@repo/ui/code";
+import styles from "./page.module.css";
+import { Button } from "@repo/ui/button";
+
+function Gradient({
+  conic,
+  className,
+  small,
+}: {
+  small?: boolean;
+  conic?: boolean;
+  className?: string;
+}): JSX.Element {
+  return (
+    <span
+      className={[
+        styles.gradient,
+        conic ? styles.glowConic : undefined,
+        small ? styles.gradientSmall : styles.gradientLarge,
+        className,
+      ]
+        .filter(Boolean)
+        .join(" ")}
+    />
+  );
+}
+
+
+
+export default function Page(): JSX.Element {
+  return (
+    <div className="flex justify-between">
+      <div>user payment app </div>
+      <div><button>logout</button></div>
+    </div>
+  );
+}
