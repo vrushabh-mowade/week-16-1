12a3ab2849d56b3d49ef8201c5ef71837421e758
vrushabh-mowade <vrushabhmowade@gmail.com>
2024-04-04 10:06:59 +0530

    dfdf

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -31,9 +31,14 @@ function Gradient({
 
 export default function Page(): JSX.Element {
   return (
-    <div className="flex justify-between">
+    <div className="flex justify-between text-2xl">
       <div>user payment app </div>
       <div><button>logout</button></div>
     </div>
   );
 }
+
+
+// DATABASE_URL="postgresql://vrushabhmowade:qigoTJRs47WB@ep-super-night-a5m7b5g1.us-east-2.aws.neon.tech/database?sslmode=require"
+
+// "prisma://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiMGRmZTdiNjAtZWRkMC00MjJlLTlmOWMtNjAzZTU5YzM1ZTQ4IiwidGVuYW50X2lkIjoiNTUyMjQzMTc5YTNmZmE0ZmQ2NDRhYzA5MTVjZTZlMTQ0YWM3ZjIyODIyMjAyMDI2YzczZjE0ZmRhYjM2MTFiNCIsImludGVybmFsX3NlY3JldCI6IjM0Njg0ZDAzLTViZDMtNDNhYy04ODUyLTI3NGQ4ZDNhZWE0OCJ9.IeD2-no2uhhl-imq_7u1yWX9GSe4t0tKoWm-aMVXDSA"
